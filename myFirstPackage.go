1cb7d4ee1582d09e981c687f0091a31f658c41dd
bashirahsan <62828666+bashirahsan@users.noreply.github.com>
2020-03-29 23:46:06 +0500

    Update myFirstPackage.go

--- myFirstPackage.go
+++ myFirstPackage.go
@@ -3,17 +3,17 @@ package pdc-quiz02-package
 
 import "fmt"
 
-func myPackage(choice int) int {
+func myPackage(Choice int) int {
 		cases := []string{"Pakistan has 1526 cases.","South Korea has 9583 cases.","Farance has 37575 cases.","Go Cororna GO"}
-		if choice == 1 {
-			fmt.Println(cases[0])
-		}else if choice == 2 {
-			fmt.Println(cases[1])
-		}else if choice == 3 {
-			fmt.Println(cases[2])
-		}else if choice == 4 {
-			fmt.Println(cases[3])
-		}else if choice == 0 {
+		if Choice == 1 {
+			fmt.Println(Cases[0])
+		}else if Choice == 2 {
+			fmt.Println(Cases[1])
+		}else if Choice == 3 {
+			fmt.Println(Cases[2])
+		}else if Choice == 4 {
+			fmt.Println(Cases[3])
+		}else if Choice == 0 {
 			fmt.Println("You can't exit without knowing cases for all countries named in the menu.")
 		}else{
 			fmt.Println("Option not valid.")
